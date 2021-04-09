4da671698fba51354fb42e793911ccce4cce36bb
Marcos Francisco Picosque <mpicosque@gmail.com>
2021-04-09 16:19:23 -0300

    Added final Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import Routes from 'Routes';
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Routes />
+  </div>
+);
 
 export default App;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'core/components/ButtonIcon/ButtonIcon';
+import "./styles.css"
 
-const Home = () => {
-  return (
-    <div className="Home">
-      <h1>Home page running</h1>
-      <Link to="/Search" >
-          <ButtonIcon text = "Começar" />
+const Home = () => (
+  <div className="Home-conteiner">
+    <div className="Home-content">
+      <h1 className="Home-content-title">
+        Desafio do Capítulo 3, <br />
+        Bootcamp DevSuperior
+      </h1>
+      <p className="Home-content-subtitle">
+        Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.
+      </p>
+      <p className="Home-content-subtitle">
+        Favor observar as instruções passadas no capítulo sobre a  <br />
+        elaboração deste projeto.
+      </p>
+      <p className="Home-content-subtitle">
+        Este design foi adaptado a partir de Ant Design System for Figma, de  <br />
+        Mateusz Wierzbicki: <a href="mailto:antforfigma@gmail.com">antforfigma@gmail.com</a>
+      </p>
+      <Link to="/Search">
+        <ButtonIcon text="Começar" />
       </Link>
     </div>
-  );
-}
+  </div>
+);
 
 export default Home;
